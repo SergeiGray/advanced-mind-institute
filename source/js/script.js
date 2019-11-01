d7538c2732b1ef9b9692b845b97c75618dfbaf3c
SergeiGray <borodat_for@mail.ru>
2019-11-01 06:01:19 +0300

    Стартовая страница. Первые правки. Десктоп

--- source/js/script.js
+++ source/js/script.js
@@ -14,12 +14,12 @@
         nextText: "",
         itemWidth: 140,
         itemMargin: 60,
-        minItems: 5,
+        minItems: 1,
         maxItems: 5,
         controlNav: false,
         directionNav: true,
         touch: true,
-        move: 5
+        move: 1
       });
       $('.feedback_slider').flexslider({
         selector: ".feedback_slider_container > li",
@@ -105,7 +105,35 @@
     });
   };
 
-  var rellax = new Rellax('.rellax');
+  var scrollBarСustomization = function () {
+    $(document).ready(function() {
+      $('.feedback_slider_text').niceScroll({
+        cursorborder: "none",
+        cursoropacitymin: "0",
+        cursorwidth: "4px",
+        scrollspeed: "30",
+        mousescrollstep: "10",
+        background: "#b1b1b1",
+        cursorminheight: "97"
+      });
+    });
+
+    $('.feedback_flipping .flex-next, .feedback_flipping .flex-prev').click(function(evt) {
+        $('.feedback_slider_text').getNiceScroll().remove();
+        $('.feedback_slider_text').niceScroll({
+          cursorborder: "none",
+          cursoropacitymin: "0",
+          cursorwidth: "4px",
+          scrollspeed: "30",
+          mousescrollstep: "10",
+          background: "#b1b1b1",
+          cursorminheight: "97"
+        });
+    });
+  };
+
+  var rellaxFirst = new Rellax('.parallax_first_container');
+  var rellaxSecond = new Rellax('.parallax_second_container');
 
   var getTimer = function () {
     jQuery(document).ready(function($) {
@@ -143,35 +171,46 @@
     });
   };
 
-$(document).ready(function(){
-  $("#jquery_jplayer_1").jPlayer({
-    ready: function () {
-      $(this).jPlayer("setMedia", {
-        mp3: "resources/Linkin_Park_-_Numb.mp3",
-        oga: ""
-      });
-    },
-    swfPath: "/js",
-    supplied: "mp3,oga"
+  $(document).ready(function(){
+    $("#jquery_jplayer_1").jPlayer({
+      ready: function () {
+        $(this).jPlayer("setMedia", {
+          mp3: "resources/Linkin_Park_-_Numb.mp3",
+          oga: ""
+        });
+      },
+      swfPath: "/js",
+      supplied: "mp3,oga"
+    });
   });
-});
 
-var showHeaderMenu = function () {
-  $('.header_navigation_container').hover(function(evt) {
-    $(evt.currentTarget).parent('.header_navigation_item').find('.header_navigation_link').addClass('active');
-  }, function(evt) {
-    $(evt.currentTarget).parent('.header_navigation_item').find('.header_navigation_link').removeClass('active');
-  });
-};
+  var showHeaderMenu = function () {
+    $('.header_navigation_container').hover(function(evt) {
+      $(evt.currentTarget).parent('.header_navigation_item').find('.header_navigation_link').addClass('active');
+    }, function(evt) {
+      $(evt.currentTarget).parent('.header_navigation_item').find('.header_navigation_link').removeClass('active');
+    });
+    $('.header_navigation_drop_down').click(function(evt) {
+      evt.preventDefault();
+    });
+  };
 
-var showLanguageMenu = function () {
-  $('.header_language_button').click(function(evt) {
-    $('.header_language').toggleClass('active');
-  });
-};
+  var showLanguageMenu = function () {
+    $('.header_language_switch').click(function(evt) {
+      evt.preventDefault();
+      $('.header_language').toggleClass('active');
+    });
+    $(document).mouseup(function(evt) {
+      var div = $('.header_language_switch');
+      if (!div.is(evt.target) && div.has(evt.target).length === 0) {
+       $('.header_language').removeClass('active');
+      }
+    });
+  };
 
-  getTimer();
   hangFlexslider();
+  scrollBarСustomization();
+  getTimer();
   showHeaderMenu();
   showLanguageMenu();
 
